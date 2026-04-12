7d6e83db9631b4f10916790ff5f83288898d7cef
Timo Tyrakowski <tt@benfilo.de>
2026-04-12 10:17:38 +0200

    fix: wrap app in GestureHandlerRootView for swipe gestures

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { StatusBar } from 'expo-status-bar'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import AppNavigator from './src/navigation/AppNavigator'
 import { startSyncLoop } from './src/lib/sync'
 import { checkForUpdate } from './src/lib/updater'
@@ -7,9 +8,9 @@ import { checkForUpdate } from './src/lib/updater'
 export default function App() {
   useEffect(() => { startSyncLoop(); checkForUpdate() }, [])
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar style="light" />
       <AppNavigator />
-    </>
+    </GestureHandlerRootView>
   )
 }
